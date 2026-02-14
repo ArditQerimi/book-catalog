e109188a09b0dd176aa28ad94b9d803d4f680fc5
Ardit <98714739+ArditQerimi@users.noreply.github.com>
2026-02-14 13:39:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  }
-});
+  base: '/book-catalog/'  // Add this line for GitHub Pages
+})
+// export default defineConfig({
+//   server: {
+//     port: 3000,
+//     host: '0.0.0.0',
+//   },
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, '.'),
+//     }
+//   }
+// });
+
+
+
